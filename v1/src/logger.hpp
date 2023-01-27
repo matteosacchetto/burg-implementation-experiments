3d049360bbc52d37107b4891efaafe28ebb70f31
Matteo Sacchetto <56300116+matteosacchetto@users.noreply.github.com>
2023-01-27 21:11:46 +0100

    fix: use cerr instead of cout

--- v1/src/logger.hpp
+++ v1/src/logger.hpp
@@ -35,22 +35,22 @@ private:
 public:
     static inline void info(const std::string& s, std::size_t indent = 0)
     {
-        std::cout << std::string(isTTY(std::cout) ? "\u001B[44m\u001B[37m\u001B[1m  INFO  \u001B[22m\u001B[39m\u001B[49m " : "|  INFO  | ") << logger::format(s, 9 + indent + (isTTY(std::cout) ? 0 : 2));
+        std::cerr << std::string(isTTY(std::cerr) ? "\u001B[44m\u001B[37m\u001B[1m  INFO  \u001B[22m\u001B[39m\u001B[49m " : "|  INFO  | ") << logger::format(s, 9 + indent + (isTTY(std::cerr) ? 0 : 2));
     };
 
     static inline  void error(const std::string& s, std::size_t indent = 0)
     {
-        std::cout << std::string(isTTY(std::cout) ? "\u001B[41m\u001B[37m\u001B[1m  ERROR  \u001B[22m\u001B[39m\u001B[49m " : "|  ERROR  | ") << logger::format(s, 10 + indent + (isTTY(std::cout) ? 0 : 2));
+        std::cerr << std::string(isTTY(std::cerr) ? "\u001B[41m\u001B[37m\u001B[1m  ERROR  \u001B[22m\u001B[39m\u001B[49m " : "|  ERROR  | ") << logger::format(s, 10 + indent + (isTTY(std::cerr) ? 0 : 2));
     };
 
     static inline  void warning(const std::string& s, std::size_t indent = 0)
     {
-        std::cout << std::string(isTTY(std::cout) ? "\u001B[43m\u001B[30m\u001B[1m  WARN  \u001B[22m\u001B[39m\u001B[49m " : "|  WARN  | ") << logger::format(s, 9 + indent + (isTTY(std::cout) ? 0 : 2));
+        std::cerr << std::string(isTTY(std::cerr) ? "\u001B[43m\u001B[30m\u001B[1m  WARN  \u001B[22m\u001B[39m\u001B[49m " : "|  WARN  | ") << logger::format(s, 9 + indent + (isTTY(std::cerr) ? 0 : 2));
     };
 
     static inline  void success(const std::string& s, std::size_t indent = 0)
     {
-        std::cout << std::string(isTTY(std::cout) ? "\u001B[42m\u001B[37m\u001B[1m  SUCCESS  \u001B[22m\u001B[39m\u001B[49m " : "|  SUCCESS  | ") << logger::format(s, 12 + indent + (isTTY(std::cout) ? 0 : 2));
+        std::cerr << std::string(isTTY(std::cerr) ? "\u001B[42m\u001B[37m\u001B[1m  SUCCESS  \u001B[22m\u001B[39m\u001B[49m " : "|  SUCCESS  | ") << logger::format(s, 12 + indent + (isTTY(std::cerr) ? 0 : 2));
     };
 };
 
